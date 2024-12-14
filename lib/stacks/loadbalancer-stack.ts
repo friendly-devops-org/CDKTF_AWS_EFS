babb4c8d39314df9ca7c090dc4e1051dca60dffd
joj <young2k15@tutanota.com>
2024-12-14 08:30:49 -0500

    use only one subnet

--- lib/stacks/loadbalancer-stack.ts
+++ lib/stacks/loadbalancer-stack.ts
@@ -28,7 +28,7 @@ export class LoadBalancerStack extends AwsStackBase {
             securityGroups: [props.securityGroup],
             namePrefix: "cl-",
             loadBalancerType: "application",
-            subnets: [`${process.env.SUBNET}`],
+            subnets: [`${process.env.SUBNET}`, `${process.env.SUBNET_2}`],
             idleTimeout: 60,
             ipAddressType: "dualstack",
         })
